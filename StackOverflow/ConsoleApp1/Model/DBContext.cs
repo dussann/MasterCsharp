7d82e4467b1069e856a43043a2c4ab305fe14d89
Dusan <dussann1989@gmail.com>
2022-05-24 01:00:58 +0200

    finish CRUD ref functions

--- StackOverflow/ConsoleApp1/Model/DBContext.cs
+++ StackOverflow/ConsoleApp1/Model/DBContext.cs
@@ -10,20 +10,23 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1.Model
 {
-    class DBContext: DbContext
+    public class DBContext: DbContext
     {
         
         public DbSet<User> User { get; set; }
         public DbSet<Question> Question { get; set; }
         public DbSet<Answer> Answer { get; set; }
-        
+        /*public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+            
+        }*/
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var serverVersion = new MySqlServerVersion(new Version(8, 0, 23));
-            optionsBuilder.UseMySql("server=localhost;user=root;database=soentity;password=root;port=3306", serverVersion);
+            optionsBuilder.UseMySql("server=localhost;user=root;database=soentity;password=root;port=3306", serverVersion);            
             // optionsBuilder.UseMySql("server=localhost;user=root;database=soentity;password=root;port=3306", serverVersion).LogTo(Console.WriteLine, LogLevel.Information);
-            // optionsBuilder.UseMySql("server=localhost;user=root;database=StackOwerflow;password=root;port=3306", serverVersion);
-
+            //optionsBuilder.UseMySql(@"server=localhost;user=root;database=soentity;password=root;port=3306", serverVersion, o=>o.MaxBatchSize(100).MinBatchSize(1));
+            
         }
        
     }
